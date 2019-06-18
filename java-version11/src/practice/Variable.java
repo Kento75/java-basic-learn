00d20bf7de3eee5e2d3366ba1e903ab08ad6050a
Kento75 <kento2github@gmail.com>
2019-06-18 18:21:33 +0900

    Add valiable

---
+++ java-version11/src/practice/Variable.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class Variable {
+    public static void main(String[] args) {
+        String name = "Kento75";
+        System.out.println("Hello " + name);
+
+        // 文字列 String
+        String number = "1" + "2";
+        System.out.println(number);
+
+        // プリミティブ型(整数） int
+        int numberInt = 1 + 2;
+        System.out.println(numberInt);
+
+        // boolean true(真) or false(偽) の２値しか存在しない
+        boolean flag = true;
+        boolean flag2 = false;
+    }
+}
