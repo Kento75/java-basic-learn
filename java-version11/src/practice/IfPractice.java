8ffa2bd6740fc4377b51ae85a57bc34301fc0f0b
Kento75 <kento2github@gmail.com>
2019-06-21 16:44:23 +0900

    Add If

---
+++ java-version11/src/practice/IfPractice.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class IfPractice {
+    public static void main(String[] args) {
+        int number = 9;
+
+        if (number == 10) {
+            System.out.println("10です");
+        } else if (number == 9) {
+            System.out.println("9です");
+        } else {
+            System.out.println("9でも10でもないです");
+        }
+    }
+}
